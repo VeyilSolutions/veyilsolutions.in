7bcb82eec7cee7101f5871316301d60bf3f66d56
Veyil Solutions <logananthan283@gmail.com>
2026-01-02 14:51:21 +0530

    New progress bar

--- script.js
+++ script.js
@@ -391,3 +391,15 @@ window.addEventListener('load', () => {
         document.body.style.opacity = '1';
     }, 100);
 });
+
+//-------------------------------------------------------------------------------
+//Progress
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const progress = (scrollTop / scrollHeight) * 100;
+  document.getElementById("scroll-progress").style.width = progress + "%";
+});
